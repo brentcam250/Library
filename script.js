46b800e482a7920dc9d74d9237565fe28434eac5
Brent Cameron <bjcameron@gmail.com>
2021-11-19 07:58:32 -0700

    wrote book constructor

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 let myLibrary = [];
+
+function Book(title, author, publishedDate, read = false){
+  //constructor
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read
+}
+
+function addBookToLibrary(){
+  
+}

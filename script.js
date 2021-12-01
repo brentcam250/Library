215026541927c5db1bc95e42d26f2d90cc424363
Brent Cameron <bjcameron@gmail.com>
2021-12-01 07:32:07 -0700

    got books to display

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const container = document.querySelector(".container");
 const form = document.getElementById("add-book-form");
 
 
-function Book(title, author, publishedDate, read = false){
+function Book(title, author, pages, read = false){
   //constructor
   this.title = title,
   this.author = author,
@@ -13,13 +13,23 @@ function Book(title, author, publishedDate, read = false){
 }
 
 function addBookToLibrary(book){
-  myLibrary.push(book);
-  console.log(myLibrary);
-  console.log(book);
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-  localStorage.setItem('myLibrary', (myLibrary));
+  // book_string = `"title": ${book.title}, "author": ${book.author}, "pages": ${(book.pages)}, "read": ${book.read}`;
+  // console.log(book_string);
 
-  console.log(localStorage.getItem('myLibrary'));
+  // myLibrary.push(book_string);
+  myLibrary.push(book)
+  console.log(book);
+  // console.log(myLibrary);
+  // console.log(book);
+  // localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  // localStorage.setItem('myLibrary', (myLibrary));
+
+  // console.log(localStorage.getItem('myLibrary'));
+  // books = localStorage.getItem('myLibrary');
+  // let books = JSON.parse(string);
+  // new_book = books[0];
+  // console.log(new_book)
+
 
 }
 function openForm() {
@@ -54,24 +64,32 @@ form.addEventListener("submit", function(event){
 })
 
 function displayBooks(){
-  // books = (localStorage.getItem("myLibrary"));
+  // let test = (localStorage.getItem("myLibrary"));
   // let string = '{title: "asdf", author: "asdf", pages: input#pages, read: "on"}';
-  let string = '[1, 5, "false"]';
+  // let string = '[1, 5, "false"]';
   // console.log(typeof books);
   // let string = (localStorage.getItem("myLibrary"));
-  let books = JSON.parse(string);
-  console.log(books)
-  let list = document.getElementById('books-table');
+  // let books = JSON.parse(string);
+  // console.log(`testing ${books}`)
+  // console.log(books.title);
+  let booksTable = document.getElementById('books-table');
   let li;
   // console.log(books);
   // let books;
   // console.log(books);
-  for (book in books ){
-    console.log(book)
-    li = document.createElement('li');
-    li.appendChild(document.createTextNode(book.title));
-    list.appendChild(li);
-    // console.log(li);
+  let row;
+  let i = 0;
+  // for (book in myLibrary ){
+  while (i < myLibrary.length){
+    // console.log(myLibrary[i])
+    row = booksTable.insertRow(i);
+      titleCell = row.insertCell(0)
+        titleCell.innerHTML = myLibrary[i].title
+      authorCell = row.insertCell(1)
+        authorCell.innerHTML = myLibrary[i].author
+      pagesCell = row.insertCell(2)
+        pagesCell.innerHTML = myLibrary[i].pages
+    i ++;
   }
   // console.log(books)
 }
